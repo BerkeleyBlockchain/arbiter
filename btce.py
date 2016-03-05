6e2d005c127c1fc5c836bbd4f8c0d84fa4c01c24
axu217 <albertxu217@gmail.com>
2016-03-04 19:27:03 -0800

    add btce

--- btce.py
+++ btce.py
@@ -26,3 +26,29 @@ def btce_order_book():
     
   return canonicalize(assocArray)
 
+def btce_load():
+  url = "https://btc-e.com/api/3/ticker/btc_usd
+
+  # Response object
+  urlResponse = urllib.request.urlopen(url)
+
+  # Read response
+  bitRead = urlResponse.read()
+
+  # Convert bitstream response to string
+  stringResponse = bitRead.decode("utf-8")
+
+  # Load the associative array from the string
+  IOobj = io.StringIO(stringResponse)
+  assocArray = json.load(IOobj)
+
+  return assocArray
+
+def btce_last():
+  return btce_load()['btc_usd']['last']
+
+def btce_volume():
+  return btce_load()['btc_usd']['vol']
+
+
+

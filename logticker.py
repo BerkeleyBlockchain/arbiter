760555ebd1eed951363c8398c7f3ae753c30b3bc
Neel Kant <kantneel@berkeley.edu>
2016-04-09 18:09:53 -0700

    updated logticker

--- logticker.py
+++ logticker.py
@@ -1,12 +1,16 @@
 from exchange import *
 from coinbase import coinbase_order_book
+from cexio import cexio_order_book
 from datetime import datetime
 
 def logticker():
 	coinbase = Exchange("coinbase", coinbase_order_book)
+	cexio = Exchange("cexio", cexio_order_book)
 	f = open("logticker.txt", 'a')
 	time = str(datetime.now())
-	f.write("Bid at " + time + " for Coinbase is: " + coinbase.bid() + '\n')
+	f.write(time + "\n")
+	f.write("Coinbase - Bid: " + coinbase.bid() + " Ask: " + coinbase.ask() + "\n")
+	f.write("Cexio - Bid: " + str(cexio.bid()) + " Ask: " + str(cexio.ask()) + "\n\n")
 	f.close()
 
 logticker()
